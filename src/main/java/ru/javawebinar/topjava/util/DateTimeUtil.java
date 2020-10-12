69e72728accce30456a16a3415323c716e018e53
space125 <kurilov@list.ru>
2020-10-12 19:49:12 +0300

    HW2 base + optional review code 3

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -8,15 +8,15 @@ import java.time.temporal.ChronoUnit;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static <T extends Comparable<T>> boolean isBetweenHalfOpen( T value, T start, T end) {
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T value, T start, T end) {
         return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
     }
 
-    public static LocalDateTime atStartOfDayMin(LocalDate ld){
+    public static LocalDateTime atStartOfDayMin(LocalDate ld) {
         return ld == null ? LocalDateTime.MIN : ld.atStartOfDay();
     }
 
-    public static LocalDateTime atStartOfNextDayMax(LocalDate ld){
+    public static LocalDateTime atStartOfNextDayMax(LocalDate ld) {
         return ld == null ? LocalDateTime.MAX : ld.plus(1, ChronoUnit.DAYS).atStartOfDay();
     }
 
