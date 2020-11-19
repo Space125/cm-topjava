8437c0a578f1d6bdea29e43c623a87372d7cedbd
space125 <kurilov@list.ru>
2020-11-19 18:17:03 +0300

    Revert "Apply 7_07_HW6_optional_jdbc_validation.patch"
    This reverts commit 806e5bc8

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -13,7 +13,6 @@ import org.springframework.util.CollectionUtils;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.util.ValidationUtil;
 
 import java.util.*;
 
@@ -42,8 +41,6 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     @Transactional
     public User save(User user) {
-        ValidationUtil.validate(user);
-
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
 
         if (user.isNew()) {
