66dc747a421bf9ea673b0cbaf7b51e746a2d0d95
space125 <kurilov@list.ru>
2020-12-03 16:00:57 +0300

    HW8 Base + Optional
    Fix topjava.meals.js sorted table.

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -29,7 +29,7 @@ $(function () {
             "order": [
                 [
                     0,
-                    "asc"
+                    "desc"
                 ]
             ]
         }),
